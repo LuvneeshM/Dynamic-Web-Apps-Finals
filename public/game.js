7ed10a2c809ddb660f28a285e30444161a36b972
LuvneeshM <lm3300@nyu.edu>
2019-04-29 00:27:17 -0400

    graphics and multiplayer controls in place

---
+++ public/game.js
@@ -0,0 +1,160 @@
+var socket = io();
+
+//controlls
+var player_movement1 = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+var player_movement2 = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+var player_shooting1 = false
+var player_shooting2 = false
+document.addEventListener('keydown', function(event) {
+        switch (event.keyCode) {
+            case 65: // A
+              player_movement1.left = true;
+              break;
+            case 87: // W
+              player_movement1.up = true;
+              break;
+            case 68: // D
+              player_movement1.right = true;
+              break;
+            case 83: // S
+              player_movement1.down = true;
+              break;
+            //player 2
+            case 37: // 
+              player_movement2.left = true;
+              break;
+            case 38: // W
+              player_movement2.up = true;
+              break;
+            case 39: // D
+              player_movement2.right = true;
+              break;
+            case 40: // S
+              player_movement2.down = true;
+              break;
+        }
+    });
+document.addEventListener('keyup', function(event) {
+        switch (event.keyCode) {
+            case 65: // A
+              player_movement1.left = false;
+              break;
+            case 87: // W
+              player_movement1.up = false;
+              break;
+            case 68: // D
+              player_movement1.right = false;
+              break;
+            case 83: // S
+              player_movement1.down = false;
+              break;
+            case 32: //space
+              player_shooting1 = true;
+              break;
+             //player 2
+            case 37: // A
+              player_movement2.left = false;
+              break;
+            case 38: // W
+              player_movement2.up = false;
+              break;
+            case 39: // D
+              player_movement2.right = false;
+              break;
+            case 40: // S
+              player_movement2.down = false;
+              break;
+            case 16: //shift
+              player_shooting2 = true;
+              break;
+        }
+        
+});
+
+//user connected, sending to server keyboard state 
+socket.emit('new player');
+//type of player
+socket.on('pmove')
+//sending the state of movement 30 times a second
+setInterval(function() {
+  socket.emit('movement', player_movement1, player_movement2);
+}, 1000 / 30);
+setInterval(function() {
+    if(player_shooting1){
+        socket.emit("shoot", 1)
+        player_shooting1 = false
+    }
+    if(player_shooting2){
+        socket.emit("shoot", 2)
+        player_shooting2 = false
+    }
+}, 1000 / 30);
+
+//draw player
+function drawPlayer(context, player){
+
+    var img = new Image();
+    img.onload = function () {
+        context.drawImage(img, player.x, player.y, player.w, player.h);
+    }
+    img.src = player.pDir
+ 
+    // context.fillStyle = player.pcolor;
+    // context.beginPath();
+    // context.arc(player.x, player.y, player.r, 0, 2 * Math.PI);
+    // context.fill();
+}
+
+//get the board
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players, bullets) {
+  context.clearRect(0, 0, 800, 600);
+  for (var id in players) {
+    var player = players[id];
+    if(player.health != 0){
+        //check if collision with bullet
+        for(var i = 0; i < bullets.length; i++){
+            var bullet = bullets[i]
+            if (bullet.bid != player.pid){
+                bleft = bullet.x - bullet.r
+                bright = bullet.x + bullet.r
+                btop = bullet.y - bullet.r
+                bbot = bullet.y + bullet.r
+
+                pleft = player.x - player.r
+                pright = player.x + player.r
+                ptop = player.y - player.r
+                pbot = player.y + player.r
+
+                if (bright > pleft && bright < pright && bbot > ptop && bbot < pbot){
+                    console.log("HIIIITTTT")
+                    player.health = 0
+                    socket.emit("pdeath", player.pid)
+                    socket.emit("bdeath", i)
+                }
+            }
+        }
+        drawPlayer(context, player)   
+    }
+  }
+  for(var i = 0; i < bullets.length; i++){
+    var b = bullets[i]
+    context.fillStyle = "#DC143C"
+    context.beginPath();
+    context.arc(b.x, b.y, b.r, 0, 2 * Math.PI);
+    context.fill();
+  }
+});
---
+++ server.js
@@ -0,0 +1,198 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app)
+var io = socketIO(server);
+
+port = 3000
+
+p1 = true
+p2 = true
+
+function Player(id, pN){
+    this.moveX = function(value){
+        this.x += value
+        this.dirX = value/(Math.abs(value))
+        this.dirY = 0
+    }
+    this.moveY = function(value){
+        this.y += value
+        this.dirY = value/(Math.abs(value))
+        this.dirX = 0
+    }
+
+    this.setPlayerColor = function(){
+        var letters = "0123456789ABCDEF"
+        var color = '#'
+        for (var i = 0; i < 6; i++) 
+            color += letters[(Math.floor(Math.random() * letters.length))]; 
+        return color
+    }
+
+    this.pN = pN
+    this.pid = id
+    if(this.pN == 1){
+        this.x = Math.floor(Math.random() * 100 + 100)
+        this.pDir = "p1/up.png"
+    }
+    else{
+        this.x = Math.floor(Math.random() * 200 + 400)
+        this.pDir = "p2/up.png"
+    }
+    this.y = 300
+    this.dirX = 0
+    this.dirY = -1
+    this.health = 2
+    this.r = 10
+    this.w = 50
+    this.h = 50
+    this.pcolor = this.setPlayerColor()
+    console.log(this.pcolor)
+
+}
+
+function Bullet(x,y,dirX,dirY,id,pr){
+    this.moveX = function(value){
+        this.x += value
+        // this.dirX = value/(Math.abs(value))
+    }
+    this.moveY = function(value){
+        this.y += value
+        // this.dirY = value/(Math. abs(value))
+    }
+
+    this.bid = id
+    this.dirX = dirX
+    this.dirY = dirY
+    this.r = 5
+    this.x = (x+pr*this.dirX)
+    this.y = (y+pr*this.dirY)
+}
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//routing
+app.get("", function(req, res){
+    res.sendFile(__dirname+'/index.html')
+})
+
+var players = {}
+var sockets = []
+var bullets = []
+//communcations
+io.on('connection', function(socket){
+    //new player
+    socket.on('new player', function(){
+        if(p1){
+            players[socket.id] = new Player(socket.id, 1)
+            p1 = false
+            // io.sockets.emit("pmove", 1)
+            sockets.push(socket.id)
+        } else if (p2){
+            players[socket.id] = new Player(socket.id, 2)
+            p2 = false
+            // io.sockets.emit("pmove", 2)
+            sockets.push(socket.id)
+        }
+    })
+    //movement
+    socket.on('movement', function(p1d, p2d) {
+        for(var i = 0; i < sockets.length; i++){
+            var player = players[sockets[i]];
+            if(player.pN == 1){
+                if (p1d.left) {
+                    player.moveX(-5);
+                    player.pDir = "p1/left.png"
+                }
+                else if (p1d.up) {
+                    player.moveY(-5);
+                    player.pDir = "p1/up.png"
+                }
+                else if (p1d.right) {
+                    player.moveX(5);
+                    player.pDir = "p1/right.png"
+                }
+                else if (p1d.down) {
+                    player.moveY(5);
+                    player.pDir = "p1/down.png"
+                }
+            }
+            else if (player.pN == 2){
+                if (p2d.left) {
+                    player.moveX(-5);
+                    player.pDir = "p2/left.png"
+                }
+                else if (p2d.up) {
+                    player.moveY(-5);
+                    player.pDir = "p2/up.png"
+                }
+                else if (p2d.right) {
+                    player.moveX(5);
+                    player.pDir = "p2/right.png"
+                }
+                else if (p2d.down) {
+                    player.moveY(5);
+                    player.pDir = "p2/down.png"
+                }
+            }
+        }
+    });
+    //shooting
+    socket.on('shoot', function(pN){
+        for(var i = 0; i < sockets.length; i++){
+            var player = players[sockets[i]]
+            if(pN == player.pN){
+                bullets.push(new Bullet(player.x+player.w/2, player.y+player.w/2, player.dirX, player.dirY, player.id, player.w))
+                break
+            }
+        }       
+    })
+    //death
+    socket.on("pdeath", function(pid){
+        if(players[pid]){
+            if(players[pid].pN == 1){
+                p1 = true
+            } else if (players[pid].pN == 2){
+                p2 = true
+            }
+            sockets.splice( sockets.indexOf(pid), 1 )
+            delete players[pid]
+        }
+    })
+    socket.on("bdeath", function(pos){
+        bullets.splice(pos,1)
+    })
+    //disconnect
+    socket.on('disconnect', function(){
+        if(players[socket.id]){
+            if(players[socket.id].pN == 1){
+                p1 = true
+            } else if (players[socket.id].pN == 2){
+                p2 = true
+            }
+            sockets.splice( sockets.indexOf(socket.id), 1 )
+            delete players[socket.id]
+        }
+    })
+})
+
+//update the bullets and move them
+setInterval(function(){
+    for(var i = 0; i < bullets.length; i++){
+        var b = bullets[i]
+        b.moveX(b.dirX*6)
+        b.moveY(b.dirY*6)
+    }
+}, 1000 / 30)
+
+//think of this as the draw function
+setInterval(function() {
+  io.sockets.emit('state', players, bullets);
+}, 1000 / 60);
+
+server.listen(port, function(){
+  console.log('listening on *:'+port);
+});
